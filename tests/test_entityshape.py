8f2e7cf414bb0e91f179ad4f6e5ee59909d9c81a
Dennis Priskorn <egils@riseup.net>
2023-06-23 11:35:20 +0200

    rewrite: Combine with PyEntityshape to avoid unnecessary http requests.

---
+++ tests/test_entityshape.py
@@ -0,0 +1,44 @@
+from unittest import TestCase
+
+from entityshape import ApiError, EntityShape, Result
+
+
+class TestEntityShape(TestCase):
+    def test_get_result_invalid_eid(self):
+        e = EntityShape(eid="eeeE1", lang="en", qid="Q1")
+        with self.assertRaises(ApiError):
+            e.get_result()
+        # print(e.result)
+        # assert e.result != {}
+
+    def test_get_result_invalid_lang(self):
+        """The API does not seem to check whether the lang code is valid or not"""
+        e = EntityShape(eid="E376", lang="eeeeen", qid="Q119853967")
+        # with self.assertRaises(ApiError):
+        e.get_result()
+        assert e.result.is_valid is True
+        # print(e.result)
+        # assert e.result != {}
+
+    def test_get_result_invalid_qid(self):
+        e = EntityShape(eid="E1", lang="en", qid="qqqqQ1")
+        with self.assertRaises(ApiError):
+            e.get_result()
+        # print(e.result)
+        # assert e.result != {}
+
+    def test_get_result_valid(self):
+        e = EntityShape(eid="E376", lang="en", qid="Q119853967")
+        e.get_result()
+        print(e.result)
+        assert isinstance(e.result, Result)
+        assert e.result.is_valid is True
+
+    def test_get_result_invalid_wrong_schema(self):
+        e = EntityShape(eid="E375", lang="en", qid="Q119853967")
+        e.get_result()
+        print(e.result)
+        assert isinstance(e.result, Result)
+        assert e.result.is_valid is False
+        assert e.result.required_properties_that_are_missing == {"P2043"}
+        assert e.result.properties_that_are_not_allowed == {"P912", "P625", "P276"}

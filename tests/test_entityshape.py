9d7c9e324b2f2939971bf758da564d283d40c674
Dennis Priskorn <egils@riseup.net>
2023-06-25 17:15:58 +0200

    fix: Fix all tests

--- tests/test_entityshape.py
+++ tests/test_entityshape.py
@@ -8,7 +8,7 @@ class TestEntityShape(TestCase):
     def test_get_result_invalid_eid(self):
         e = EntityShape(eid="eeeE1", lang="en", qid="Q1")
         with self.assertRaises(EidError):
-            e.get_result()
+            e.validate_and_get_result()
         # print(e.result)
         # assert e.result != {}
 
@@ -16,7 +16,7 @@ class TestEntityShape(TestCase):
         """The API does not seem to check whether the lang code is valid or not"""
         e = EntityShape(eid="E376", lang="", qid="Q119853967")
         with self.assertRaises(LangError):
-            e.get_result()
+            e.validate_and_get_result()
         # assert e.result.is_valid is True
         # print(e.result)
         # assert e.result != {}
@@ -24,7 +24,7 @@ class TestEntityShape(TestCase):
     def test_get_result_invalid_qid(self):
         e = EntityShape(eid="E1", lang="en", qid="qqqqQ1")
         with self.assertRaises(QidError):
-            e.get_result()
+            e.validate_and_get_result()
         # print(e.result)
         # assert e.result != {}
 
@@ -38,8 +38,8 @@ class TestEntityShape(TestCase):
 
     def test_get_result_invalid_missing_operator(self):
         e = EntityShape(eid="E376", lang="en", qid="Q96308969")
-        e.get_result()
-        print(e.result)
+        e.validate_and_get_result()
+        # print(e.result)
         assert isinstance(e.result, Result)
         assert e.result.is_valid is False
         assert e.result.required_properties_that_are_missing == {"P137"}
@@ -56,7 +56,7 @@ class TestEntityShape(TestCase):
 
     def test_get_result_missing_statement_response(self):
         e = EntityShape(eid="E395", lang="en", qid="Q4802448")
-        e.get_result()
+        e.validate_and_get_result()
         # print(e.compare_shape_result)
         # console = Console()
         # console.print(e.result)
@@ -66,10 +66,6 @@ class TestEntityShape(TestCase):
 
     def test_get_result_weird_statement_response_party_person(self):
         e = EntityShape(eid="E395", lang="en", qid="Q20727")
-        e.get_result()
-        # print(e.compare_shape_result)
-        # console = Console()
-        # console.print(e.result)
+        e.validate_and_get_result()
         assert isinstance(e.result, Result)
-        assert e.result.is_valid is False
-        # assert e.result.properties_without_enough_correct_statements == {"P39"}
+        assert e.result.is_valid is True
